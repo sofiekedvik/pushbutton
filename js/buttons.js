604005a39594d677ad494231c29677f856382990
Sofie kedvik <sofie.kedvik@gmail.com>
2015-05-28 14:27:06 +0200

    rensat kod

--- js/buttons.js
+++ js/buttons.js
@@ -7,12 +7,12 @@ var buttons = document.getElementById('go').addEventListener('click', function()
 
     var buttonOne = document.getElementById('go');
     var changeBox = document.getElementById('box');
-console.log("Hej hej");
+
     changeBox.style.display="block";
     buttonOne.style.background="#81C88C";
     buttonOne.style.border="1px solid #4C975A";
-console.log(changeBox);
 
-    console.log(buttonOne);
+
+
 
 });
