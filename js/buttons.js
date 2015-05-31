042a595f5e81580baf7113533fd63158a451e154
Sofie kedvik <sofie.kedvik@gmail.com>
2015-05-31 15:35:02 +0200

    knapp blir grön eller röd

--- js/buttons.js
+++ js/buttons.js
@@ -1,20 +1,25 @@
 /**
  * Created by Soof on 15-05-28.
  */
-
+var counter = 1;
 
 var buttons = document.getElementById('go').addEventListener('click', function(){
 
     var buttonOne = document.getElementById('go');
     var changeBox = document.getElementById('box');
 
-    changeBox.style.display="block";
-    buttonOne.style.background="#81C88C";
-    buttonOne.style.border="1px solid #4C975A";
+    
+    counter++;
 
-
-    function myfunction(){
-        return 7 * 7 ;
+    if(counter % 2 == 0) {
+        changeBox.style.display="block";
+        buttonOne.style.background="#81C88C";
+        buttonOne.style.border="1px solid #4C975A";
+    }
+    else {
+        changeBox.style.display="none";
+        buttonOne.style.background="#ff4a46";
+        buttonOne.style.border="1px solid #962a29";
     }
 
 
